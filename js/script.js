7c7edc237e299db40bfdd8092f1d8df272d16320
00111100000 <104298321+00111100000@users.noreply.github.com>
2022-05-10 00:00:26 +0300

    odigio

---
+++ js/script.js
@@ -0,0 +1,22 @@
+(function () {
+    const header = document.querySelector('.header');
+    window.onscroll = () => {
+        if (window.pageYOffset > 50) {
+            header.classList.add('header_active');
+        } else {
+            header.classList.remove('header_active');
+        }
+    };
+}());
+
+(function () {
+    const burgerItem = document.querySelector('.burger');
+    const menu = document.querySelector('.header__nav');
+    const menuCloseItem = document.querySelector('.header__nav__close');
+    burgerItem.addEventListener('click', () => {
+        menu.classList.add('header__nav__active');
+    });
+    menuCloseItem.addEventListener('click', () => {
+        menu.classList.remove('header__nav__active');
+    });
+}());
